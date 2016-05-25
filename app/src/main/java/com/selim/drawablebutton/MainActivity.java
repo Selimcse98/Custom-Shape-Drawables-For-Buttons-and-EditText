ce2ce8785caddfb5b8ead4a6e499a9394f6b5ee5
Mohammad Selim Miah <selimcse98@gmail.com>
2016-05-25 12:43:26 +1000

    Added onClick listener

--- app/src/main/java/com/selim/drawablebutton/MainActivity.java
+++ app/src/main/java/com/selim/drawablebutton/MainActivity.java
@@ -2,6 +2,14 @@ package com.selim.drawablebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button okButton = (Button) findViewById(R.id.btn_OK);
+        Button cancelButton = (Button) findViewById(R.id.btn_Cancel);
+        final EditText searchET = (EditText) findViewById(R.id.editText1);
+
+        if (okButton != null) {
+            okButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,"Search Google for your favorite movie,mainActivity.this",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Search Google for your favorite movie, application context",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getBaseContext(),"Search Google for your favorite movie, base context",Toast.LENGTH_LONG).show();
+                }
+            });
+        }Toast.makeText(MainActivity.this,"OK button reference null",Toast.LENGTH_LONG).show();
+
+        if(cancelButton!=null){
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    searchET.setText("");
+                }
+            });
+        }
+
+        if(searchET!=null){
+            searchET.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,"You are looking for "+searchET.getText(),Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
